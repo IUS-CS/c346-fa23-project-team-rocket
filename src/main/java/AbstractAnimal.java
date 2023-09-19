3b305f235d87e672ad737bd14fe33ef27c8db8e8
ryanhursst <rhurst2001@icloud.com>
2023-09-19 19:43:03 -0400

    Implemented abstract animal class

---
+++ src/main/java/AbstractAnimal.java
@@ -0,0 +1,29 @@
+public abstract class AbstractAnimal {
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT;
+    }
+    private char icon;
+    private static int count;
+
+    /**
+     * @return animal's icon as a character
+     */
+    public abstract char toIcon(); //returns animal icon
+
+    /**
+     * Takes array of an animal's neighbors, randomly chooses an available space, and returns corresponding direction
+     * @param neighbors array of animals in adjacent tiles, 0-3 representing UP, DOWN, LEFT, or RIGHT respectively
+     * @return randomly determined direction based on available spaces
+     */
+    public abstract Direction availableMovementSpace(AbstractAnimal[] neighbors);
+
+    //public abstract void reproduce(); //creates new animal
+
+    /**
+     * @return total number of living animals
+     */
+    public abstract int total();
+}
