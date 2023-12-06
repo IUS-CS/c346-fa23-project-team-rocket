f24012e202768594aaa0e8c5a592825d09877923
morri564 <morri564@purdue.edu>
2023-12-05 19:57:48 -0500

    Documentation additions and edits

--- src/main/java/team/rocket/Entities/OrganismFactory.java
+++ src/main/java/team/rocket/Entities/OrganismFactory.java
@@ -15,41 +15,43 @@ public class OrganismFactory {
 	private OrganismFactory(){}
 
 	/**
-	 * Singleton creator
-	 * @return The single instance of the factory
+	 * Singleton creator.
+	 *
+	 * @return  the single instance of the factory
 	 */
 	public static synchronized OrganismFactory getInstance(){
-		if(singleton == null){
+		if (singleton == null) {
 			singleton = new OrganismFactory();
 		}
+
 		return singleton;
 	}
 
 	/**
-	 *  Registers an organism to the internal Hashmap
-	 *  reduces organism count to 0
-	 * @param OrganismName String name of the organism
-	 * @param organism A new object of the organism
+	 * Registers an organism to the internal Hashmap. Reduces organism count to 0.
+	 *
+	 * @param OrganismName  string name of the organism
+	 * @param organism 		a new object of the organism
 	 */
-	public void registerOrganism (String OrganismName, AbstractOrganism organism){
+	public void registerOrganism (String OrganismName, AbstractOrganism organism) {
 		OrganismName = OrganismName.toLowerCase();
-		if(!m_RegisteredOrganisms.containsKey(OrganismName)){
+		if (!m_RegisteredOrganisms.containsKey(OrganismName)) {
 			m_RegisteredOrganisms.put(OrganismName, organism);
 			organism.setCount(0);
 		}
 	}
 
 	/**
-	 * Creates a new instance of the organism with the id
-	 * @param OrganismName Organism to get the instance of
-	 * @return null if organism doesn't exist/wasn't registered, otherwise an object of AbstractOrganism type
+	 * Creates a new instance of the organism with the id.
+	 *
+	 * @param OrganismName	organism to get the instance of
+	 * @return 				null if organism doesn't exist/wasn't registered, otherwise an object of AbstractOrganism type
 	 */
 	public AbstractOrganism createOrganism (String OrganismName){
 		OrganismName = OrganismName.toLowerCase();
-		if(!m_RegisteredOrganisms.containsKey(OrganismName)){
+		if (!m_RegisteredOrganisms.containsKey(OrganismName)) {
 			return null;
 		}
 		return m_RegisteredOrganisms.get(OrganismName).getNewObjectFromExistingObject();
 	}
-
 }
